2319bc97dbf492208b478ae6e597dfd987516011
Vlad Vylotnilov <vladvyljotnikov@gmail.com>
2022-09-19 13:03:20 +0300

    project preparation

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import UsersTable from './page/UsersTable'
+
+function App() {
+    return <UsersTable />
+}
+
+export default App
---
+++ src/page/UsersTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const UsersTable = () => {
+    return <p>Template</p>
+}
+
+export default UsersTable
